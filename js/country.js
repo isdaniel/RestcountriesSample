067d5e482eba3a7ed495526644c5c7a8eefb8616
Daniel <dog830228@gmail.com>
2021-06-13 18:42:26 +0800

    1. add - 以國名搜尋功能(模糊搜尋) function. 2. enhancement display UI/UX.

--- js/country.js
+++ js/country.js
@@ -7,45 +7,45 @@ window.onload = function () {
       pageSize: 25,
       currPage: 1,
       countryName: '',
-      ASC : 1
+      ASC: 1
     },
     computed: {
-      filteredRows: function(){
-		//todo search by country name.
-		return this.rows;
+      filteredRows: function () {
+        var self = this;
+        return self.rows.filter(x=> !self.countryName || x.name.search(self.countryName) != -1);
       },
-      pageStart: function(){
-          return (this.currPage - 1) * this.pageSize;
-        },
-      totalPage: function(){
+      pageStart: function () {
+        return (this.currPage - 1) * this.pageSize;
+      },
+      totalPage: function () {
         return Math.ceil(this.filteredRows.length / this.pageSize);
       }
     },
     methods: {
-      setPage: function(index){
-        if( index <= 0 || index > this.totalPage ){
+      setPage: function (index) {
+        if (index <= 0 || index > this.totalPage) {
           return;
         }
         this.currPage = index;
       },
       orderBy: function (item) {
-          var self = this;
-          return self.rows.sort(function (obj1, obj2) {
-              var obj1 = obj1[item]
-              var obj2 = obj2[item]
+        var self = this;
+        return self.rows.sort(function (obj1, obj2) {
+          var obj1 = obj1[item]
+          var obj2 = obj2[item]
 
-              if(obj1 === obj2)
-                return 0;
-              else if(obj1 > obj2)
-                return self.ASC ;
-              else 
-                return self.ASC * -1;
-          });
+          if (obj1 === obj2)
+            return 0;
+          else if (obj1 > obj2)
+            return self.ASC;
+          else
+            return self.ASC * -1;
+        });
       }
     },
-    created: function(){
+    created: function () {
       var self = this;
-      $.get('https://restcountries.eu/rest/v2/all', function(data){
+      $.get('https://restcountries.eu/rest/v2/all', function (data) {
         self.rows = data;
       });
     }
