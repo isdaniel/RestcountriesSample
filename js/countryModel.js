fe4be8965a87fb2c408aeb3333e32620d5a42f64
Daniel <dog830228@gmail.com>
2021-06-13 20:04:05 +0800

    1. format html & js 2. finish 點擊國家名稱后顯示一個Modal，裏面顯示該國家的其他資訊

---
+++ js/countryModel.js
@@ -0,0 +1,22 @@
+
+window.onload = function () {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      vm: {},
+      countryName: ''
+    },
+    created: function () {
+      var self = this;
+      let urlParams = new URLSearchParams(window.location.search);
+      var countryName = urlParams.has('countryName') ? urlParams.get('countryName') : '';
+      var url = 'https://restcountries.eu/rest/v2/name/'+countryName+'?fullText=true'
+      $.get(url, function (data) {
+        self.vm = data[0];
+      }).fail(function() {
+        document.write('No Data Display!!');
+      });
+    }
+  });
+}
+
