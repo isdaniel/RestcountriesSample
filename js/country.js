891d90274741aa9f703c8266980630b6932cd24d
Daniel <dog830228@gmail.com>
2021-06-13 15:46:33 +0800

    finish - 目錄内為Api資料物件的屬性 - 以國名排序功能（正序、倒序） - 分頁(每頁25筆)

---
+++ js/country.js
@@ -0,0 +1,54 @@
+
+window.onload = function () {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      rows: [],
+      pageSize: 25,
+      currPage: 1,
+      countryName: '',
+      ASC : 1
+    },
+    computed: {
+      filteredRows: function(){
+		//todo search by country name.
+		return this.rows;
+      },
+      pageStart: function(){
+          return (this.currPage - 1) * this.pageSize;
+        },
+      totalPage: function(){
+        return Math.ceil(this.filteredRows.length / this.pageSize);
+      }
+    },
+    methods: {
+      setPage: function(index){
+        if( index <= 0 || index > this.totalPage ){
+          return;
+        }
+        this.currPage = index;
+      },
+      orderBy: function (item) {
+          var self = this;
+          return self.rows.sort(function (obj1, obj2) {
+              var obj1 = obj1[item]
+              var obj2 = obj2[item]
+
+              if(obj1 === obj2)
+                return 0;
+              else if(obj1 > obj2)
+                return self.ASC ;
+              else 
+                return self.ASC * -1;
+          });
+      }
+    },
+    created: function(){
+      var self = this;
+      $.get('https://restcountries.eu/rest/v2/all', function(data){
+        self.rows = data;
+      });
+    }
+  });
+}
+
